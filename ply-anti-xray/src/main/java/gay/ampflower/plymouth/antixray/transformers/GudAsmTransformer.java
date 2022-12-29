b05dade8ccb660d138f71f224aea296997d38f9e
Ampflower 🌺 <git@ampflower.gay>
2022-12-29 02:19:00 -0600

    Add method being processed at time of crash

--- ply-anti-xray/src/main/java/gay/ampflower/plymouth/antixray/transformers/GudAsmTransformer.java
+++ ply-anti-xray/src/main/java/gay/ampflower/plymouth/antixray/transformers/GudAsmTransformer.java
@@ -61,120 +61,124 @@ public class GudAsmTransformer implements Transformer {
         // I'm going 'alright, find all instances of consuming classes', then 'find the variable of the specific classes
         // I want to change', then 'find all loads of the variable and make sure it's going to outputs I want to mutate
         // anyways, or is of a different type'
-        for (var method : classNode.methods) {
-            // logger.info("Poking at {}.{}", classNode.name, method.name);
-            loadMap.clear();
-            storMap.clear();
-            mutLoad.clear();
-            for (var load : AsmUtils.findMatchingNodes(method, node -> node instanceof VarInsnNode var && var.getOpcode() >>> 4 == 1)) {
-                if (!(load instanceof VarInsnNode var)) {
-                    new Throwable("Unexpected node " + load).printStackTrace();
-                    continue;
+        for (var method : classNode.methods)
+            try {
+                // logger.info("Poking at {}.{}", classNode.name, method.name);
+                loadMap.clear();
+                storMap.clear();
+                mutLoad.clear();
+                for (var load : AsmUtils.findMatchingNodes(method, node -> node instanceof VarInsnNode var && var.getOpcode() >>> 4 == 1)) {
+                    if (!(load instanceof VarInsnNode var)) {
+                        new Throwable("Unexpected node " + load).printStackTrace();
+                        continue;
+                    }
+                    if (var.var == 0) continue;
+                    loadMap.computeIfAbsent(var.var, i -> new ArrayList<>()).add(var);
                 }
-                if (var.var == 0) continue;
-                loadMap.computeIfAbsent(var.var, i -> new ArrayList<>()).add(var);
-            }
-            for (var stor : AsmUtils.findMatchingNodes(method, node -> node instanceof VarInsnNode var && var.getOpcode() >>> 4 == 3)) {
-                if (!(stor instanceof VarInsnNode var)) {
-                    new Throwable("Unexpected node " + stor).printStackTrace();
-                    continue;
+                for (var stor : AsmUtils.findMatchingNodes(method, node -> node instanceof VarInsnNode var && var.getOpcode() >>> 4 == 3)) {
+                    if (!(stor instanceof VarInsnNode var)) {
+                        new Throwable("Unexpected node " + stor).printStackTrace();
+                        continue;
+                    }
+                    if (var.var == 0) continue;
+                    storMap.computeIfAbsent(var.var, i -> new ArrayList<>()).add(var);
                 }
-                if (var.var == 0) continue;
-                storMap.computeIfAbsent(var.var, i -> new ArrayList<>()).add(var);
-            }
-            int unsafeBefore = Type.getArgumentTypes(method.desc).length;
-            // int varAvailable = Math.max(loadMap.keySet().intStream().max().orElse(0), storMap.keySet().intStream().max().orElse(0)) + 1;
-            // boolean unsafe = false;
-            for (var $invoke : AsmUtils.findMatchingNodes(method, node -> node instanceof MethodInsnNode invoke && packetClasses.contains(invoke.owner))) {
-                if (!($invoke instanceof MethodInsnNode invoke)) {
-                    new Throwable("Unexpected node " + $invoke).printStackTrace();
-                    continue;
-                }
-                // logger.info("Poking at {}.{}{} for {}.{}{}", classNode.name, method.name, method.desc, invoke.owner, invoke.name, invoke.desc);
-                var args = Type.getArgumentTypes(invoke.desc);
-                for (int i = 0, l = args.length; i < l; i++) {
-                    if (mutateClasses.contains(args[i].getDescriptor())) {
-                        var insn = walkBackwards($invoke, l - i);
-                        if (insn instanceof VarInsnNode var) {
-                            if (var.var == 0) {
-                                logger.warn("This reference for {}.{}{} in {}.{}{}, ignoring", invoke.owner, invoke.name, invoke.desc, classNode.name, method.name, method.desc);
-                                continue; // we cannot intercept on this.
-                            } else {
-                                logger.info("Checking for STORE & LOAD on var {} within {}.{}{} in {}.{}{}", var.var, invoke.owner, invoke.name, invoke.desc, classNode.name, method.name, method.desc);
-                            }
-                            mutLoad.put(var.var, 0);
-                            // FIXME: Make this smarter, this may severely go wrong!
-                            for (var load : loadMap.get(var.var)) {
-                                pop[0] = 1;
-                                var consumer = walkForward(load, pop);
-                                if (consumer instanceof MethodInsnNode consumingMethod) {
-                                    // TODO: Check if method is allowed to receive
-                                    var consumerArgs = Type.getArgumentTypes(consumingMethod.desc);
-                                    if (consumerArgs.length >= pop[0] && consumerArgs[consumerArgs.length - pop[0]].equals(args[i])) {
-                                        logger.info("Redirecting {}.{}{}", consumingMethod.owner, consumingMethod.name, consumingMethod.desc);
-                                    } else {
-                                        logger.info("Unknown redirect {}.{}{}", consumingMethod.owner, consumingMethod.name, consumingMethod.desc);
-                                    }
-                                    // unsafe |= !consumerArgs[consumerArgs.length+pop[0]].equals(args[i]);
+                int unsafeBefore = Type.getArgumentTypes(method.desc).length;
+                // int varAvailable = Math.max(loadMap.keySet().intStream().max().orElse(0), storMap.keySet().intStream().max().orElse(0)) + 1;
+                // boolean unsafe = false;
+                for (var $invoke : AsmUtils.findMatchingNodes(method, node -> node instanceof MethodInsnNode invoke && packetClasses.contains(invoke.owner))) {
+                    if (!($invoke instanceof MethodInsnNode invoke)) {
+                        new Throwable("Unexpected node " + $invoke).printStackTrace();
+                        continue;
+                    }
+                    // logger.info("Poking at {}.{}{} for {}.{}{}", classNode.name, method.name, method.desc, invoke.owner, invoke.name, invoke.desc);
+                    var args = Type.getArgumentTypes(invoke.desc);
+                    for (int i = 0, l = args.length; i < l; i++) {
+                        if (mutateClasses.contains(args[i].getDescriptor())) {
+                            var insn = walkBackwards($invoke, l - i);
+                            if (insn instanceof VarInsnNode var) {
+                                if (var.var == 0) {
+                                    logger.warn("This reference for {}.{}{} in {}.{}{}, ignoring", invoke.owner, invoke.name, invoke.desc, classNode.name, method.name, method.desc);
+                                    continue; // we cannot intercept on this.
                                 } else {
-                                    // unsafe = true
-                                    logger.warn("Unknown consumer {}: {}, may invoke undefined behaviour.", consumer, consumer.getOpcode());
+                                    logger.info("Checking for STORE & LOAD on var {} within {}.{}{} in {}.{}{}", var.var, invoke.owner, invoke.name, invoke.desc, classNode.name, method.name, method.desc);
+                                }
+                                mutLoad.put(var.var, 0);
+                                // FIXME: Make this smarter, this may severely go wrong!
+                                for (var load : loadMap.get(var.var)) {
+                                    pop[0] = 1;
+                                    var consumer = walkForward(load, pop);
+                                    if (consumer instanceof MethodInsnNode consumingMethod) {
+                                        // TODO: Check if method is allowed to receive
+                                        var consumerArgs = Type.getArgumentTypes(consumingMethod.desc);
+                                        if (consumerArgs.length >= pop[0] && consumerArgs[consumerArgs.length - pop[0]].equals(args[i])) {
+                                            logger.info("Redirecting {}.{}{}", consumingMethod.owner, consumingMethod.name, consumingMethod.desc);
+                                        } else {
+                                            logger.info("Unknown redirect {}.{}{}", consumingMethod.owner, consumingMethod.name, consumingMethod.desc);
+                                        }
+                                        // unsafe |= !consumerArgs[consumerArgs.length+pop[0]].equals(args[i]);
+                                    } else {
+                                        // unsafe = true
+                                        logger.warn("Unknown consumer {}: {}, may invoke undefined behaviour.", consumer, consumer.getOpcode());
+                                    }
+                                }
+                                // if(unsafe) {
+                                //     if(!mutLoad.containsKey(var.var)) {
+                                //         mutLoad.put(var.var, varAvailable++);
+                                //     }
+                                // }
+                            } else if (insn instanceof MethodInsnNode supplier) {
+                                var redirect = redirectMap.get(mkType(supplier));
+                                if (redirect != null) {
+                                    transformed = true;
+                                    var old = supplier.name;
+                                    supplier.name = redirect;
+                                    logger.info("Redirected {}.{}{} in {}.{}{} to {} for {}.{}{}", supplier.owner, old, supplier.desc, classNode.name, method.name, method.desc, supplier.name, invoke.owner, invoke.name, invoke.desc);
+                                } else {
+                                    logger.info("Cannot redirect {}.{}{} in {}.{}{} for {}.{}{}", supplier.owner, supplier.name, supplier.desc, classNode.name, method.name, method.desc, invoke.owner, invoke.name, invoke.desc);
                                 }
                             }
-                            // if(unsafe) {
-                            //     if(!mutLoad.containsKey(var.var)) {
-                            //         mutLoad.put(var.var, varAvailable++);
-                            //     }
-                            // }
-                        } else if (insn instanceof MethodInsnNode supplier) {
+                        }
+                    }
+                }
+                // if(unsafe) {
+
+                // } else {
+                // for(var stor : storMap.get(var.var)) {
+                //     var supplier = walkBackwards(stor, 1);
+                //     if()
+                // }
+                // }
+                var itr = mutLoad.keySet().intIterator();
+                while (itr.hasNext()) {
+                    var var = itr.nextInt();
+                    if (!storMap.containsKey(var)) {
+                        if (var <= unsafeBefore) {
+                            logger.warn("Cannot redirect var {} in {}.{}{} as it is purely a parameter.", var, classNode.name, method.name, method.desc);
+                        } else {
+                            logger.warn("Cannot redirect var {} in {}.{}{} as it doesn't exist?! Did someone generate this class?", var, classNode.name, method.name, method.desc);
+                        }
+                    } else for (var stor : storMap.get(var)) {
+                        var $supplier = walkBackwards(stor, 1);
+                        if ($supplier instanceof MethodInsnNode supplier) {
                             var redirect = redirectMap.get(mkType(supplier));
                             if (redirect != null) {
                                 transformed = true;
                                 var old = supplier.name;
                                 supplier.name = redirect;
-                                logger.info("Redirected {}.{}{} in {}.{}{} to {} for {}.{}{}", supplier.owner, old, supplier.desc, classNode.name, method.name, method.desc, supplier.name, invoke.owner, invoke.name, invoke.desc);
+                                logger.info("Redirected {}.{}{} in {}.{}{} to {} for var {}", supplier.owner, old, supplier.desc, classNode.name, method.name, method.desc, supplier.name, var);
                             } else {
-                                logger.info("Cannot redirect {}.{}{} in {}.{}{} for {}.{}{}", supplier.owner, supplier.name, supplier.desc, classNode.name, method.name, method.desc, invoke.owner, invoke.name, invoke.desc);
+                                logger.info("Cannot redirect {}.{}{} in {}.{}{} for var {}", supplier.owner, supplier.name, supplier.desc, classNode.name, method.name, method.desc, var);
                             }
-                        }
-                    }
-                }
-            }
-            // if(unsafe) {
-
-            // } else {
-            // for(var stor : storMap.get(var.var)) {
-            //     var supplier = walkBackwards(stor, 1);
-            //     if()
-            // }
-            // }
-            var itr = mutLoad.keySet().intIterator();
-            while (itr.hasNext()) {
-                var var = itr.nextInt();
-                if (!storMap.containsKey(var)) {
-                    if (var <= unsafeBefore) {
-                        logger.warn("Cannot redirect var {} in {}.{}{} as it is purely a parameter.", var, classNode.name, method.name, method.desc);
-                    } else {
-                        logger.warn("Cannot redirect var {} in {}.{}{} as it doesn't exist?! Did someone generate this class?", var, classNode.name, method.name, method.desc);
-                    }
-                } else for (var stor : storMap.get(var)) {
-                    var $supplier = walkBackwards(stor, 1);
-                    if ($supplier instanceof MethodInsnNode supplier) {
-                        var redirect = redirectMap.get(mkType(supplier));
-                        if (redirect != null) {
-                            transformed = true;
-                            var old = supplier.name;
-                            supplier.name = redirect;
-                            logger.info("Redirected {}.{}{} in {}.{}{} to {} for var {}", supplier.owner, old, supplier.desc, classNode.name, method.name, method.desc, supplier.name, var);
                         } else {
-                            logger.info("Cannot redirect {}.{}{} in {}.{}{} for var {}", supplier.owner, supplier.name, supplier.desc, classNode.name, method.name, method.desc, var);
+                            logger.info("Cannot redirect instruction {} for var {} in {}.{}{}", $supplier, var, classNode.name, method.name, method.desc);
                         }
-                    } else {
-                        logger.info("Cannot redirect instruction {} for var {} in {}.{}{}", $supplier, var, classNode.name, method.name, method.desc);
                     }
                 }
+            } catch (Throwable t) {
+                t.addSuppressed(new Throwable("At time of processing: " + classNode.name + "." + method.name + method.desc));
+                throw t;
             }
-        }
         return transformed;
     }
 }
