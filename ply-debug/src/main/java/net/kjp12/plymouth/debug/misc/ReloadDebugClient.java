730f318286ca801f4d9ca4e999342cbbe7ff6691
Ampflower <ampflower@outlook.com>
2023-02-06 21:32:12 -0600

    Add BlockCollisionSpliterator support

--- ply-debug/src/main/java/net/kjp12/plymouth/debug/misc/ReloadDebugClient.java
+++ ply-debug/src/main/java/net/kjp12/plymouth/debug/misc/ReloadDebugClient.java
@@ -3,6 +3,9 @@ package net.kjp12.plymouth.debug.misc;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.kjp12.plymouth.debug.Fusebox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 /**
@@ -10,12 +13,20 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
  * @since ${version}
  **/
 public class ReloadDebugClient {
+
+    private static final List<Runnable> reloadables = new ArrayList<>();
+
     public static void initialise() {
         ClientCommandRegistrationCallback.EVENT.register((DISPATCHER, registryAccess) ->
                 DISPATCHER.register(literal("pdbc").then(literal("reload").executes(ctx -> {
                     Fusebox.reinit();
+                    for (final var reloadable : reloadables) reloadable.run();
                     return 1;
                 })))
         );
+    }
+
+    public static void addReloadable(Runnable runnable) {
+        reloadables.add(runnable);
     }
 }
